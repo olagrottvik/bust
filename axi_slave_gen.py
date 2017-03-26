c058daf73931d236a5fc2014bf9aa0f0dd8d04cc
Ola <ogre@localhost.localdomain>
2017-03-26 21:29:57 +0200

    print module pkg file

--- axi_slave_gen.py
+++ axi_slave_gen.py
@@ -6,7 +6,7 @@
 import os
 from reg import Reg
 
-filepath = "test/output.txt"
+filepath = "test/output.vhd"
 wregs = [Reg('drp_address', True, 0), Reg('drp_data', True, 1),
          Reg('transceiver_settings', True, 2)]
 rregs = [Reg('transceiver_status', False, 3), Reg('run_status', False, 4),
@@ -37,6 +37,11 @@ def writePkg(filepath, modName, wregs, rregs, dataSize, addrSize):
 
     ensureDirExist(filepath)
     with open(filepath, 'wt') as f:
+        print('library ieee;', file=f)
+        print('use ieee.std_logic_1164.all;', file=f)
+        print('use ieee.numeric_std.all;\n', file=f)
+        print('use work.axi_pkg.all;\n', file=f)
+        print('package ' + modName + '_pkg is\n', file=f)
         print('type t_' + modName + '_reg_write is record', file=f)
 
         for i, r in enumerate(wregs):
@@ -48,6 +53,29 @@ def writePkg(filepath, modName, wregs, rregs, dataSize, addrSize):
         for i, r in enumerate(rregs):
             print(r.name + ' : t_axi_data; -- reg ' + str(r.number), file=f)
         print('end record;\n', file=f)
+
+        print('type t_address is record', file=f)
+
+        for r in wregs:
+            print(r.name + ' : std_logic_vector(' +
+                  str(addrSize) + '-1 downto 0);', file=f)
+
+        for r in rregs:
+            print(r.name + ' : std_logic_vector(' +
+                  str(addrSize) + '-1 downto 0);', file=f)
+        print('end record t_address;\n', file=f)
+
+        print('constant reg_num : t_address :=(', file=f)
+
+        for r in wregs:
+            print(r.name + ' => ' + str(addrSize) + 'x"' + str(r.number) + '",', file=f)
+
+        for r in rregs:
+            print(r.name + ' => ' + str(addrSize) + 'x"' + str(r.number) + '",', file=f)
+
+        print(');\n', file=f)
+
+        print('end package ' + modName + '_pkg;', file=f)
 
 
 def writeGlobalPkg(filepath, dataSize, addrSize):
