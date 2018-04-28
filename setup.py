2fca52bc6aa4606a507eea8a7082cb47e2c37fcd
ogre <olagrottvik@gmail.com>
2018-04-28 17:55:49 +0200

    changed format of py header file to static class (simple reference)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except (IOError, ImportError):
 
 setup(
     name='uart',
-    version='0.5.4',
+    version='0.5.5',
     packages=['uart'],
     license='MIT',
     install_requires=[
--- uart/header.py
+++ uart/header.py
@@ -41,26 +41,25 @@ class Header(object):
     def return_python_header(self):
         s = ''
 
-        s += "class " + self.module.name.upper() + "_H():\n"
-        s += indent_string("def __init__(self):\n\n", 2)
+        s += "class " + self.module.name.upper() + "_H:\n\n"
 
-        s += indent_string("self." + self.module.name.upper() + "_BASEADDR = " + str(hex(self.module.baseaddr)), 4)
+        s += indent_string(self.module.name.upper() + "_BASEADDR = " + str(hex(self.module.baseaddr)), 2)
         s += "\n\n"
 
         for reg in self.module.registers:
-            s += indent_string('""" Register: ' + reg.name + ' """\n', 4)
-            s += indent_string("self." + reg.name.upper() + "_OFFSET = " + str(hex(reg.address)) + "\n", 4)
-            s += indent_string("self." + reg.name.upper() + "_RESET = " + reg.reset + "\n", 4)
+            s += indent_string('""" Register: ' + reg.name + ' """\n', 2)
+            s += indent_string(reg.name.upper() + "_OFFSET = " + str(hex(reg.address)) + "\n", 2)
+            s += indent_string(reg.name.upper() + "_RESET = " + reg.reset + "\n", 2)
             s += "\n"
 
             if reg.sig_type == "fields":
                 for field in reg.fields:
-                    s += indent_string('""" Field: ' + field.name + ' """\n', 4)
-                    s += indent_string("self." + reg.name.upper() + "_" + field.name.upper() + "_OFFSET = ", 4)
+                    s += indent_string('""" Field: ' + field.name + ' """\n', 2)
+                    s += indent_string(reg.name.upper() + "_" + field.name.upper() + "_OFFSET = ", 2)
                     s += str(field.pos_low) + "\n"
-                    s += indent_string("self." + reg.name.upper() + "_" + field.name.upper() + "_WIDTH = ", 4)
+                    s += indent_string(reg.name.upper() + "_" + field.name.upper() + "_WIDTH = ", 2)
                     s += str(field.length) + "\n"
-                    s += indent_string("self." + reg.name.upper() + "_" + field.name.upper() + "_RESET = ", 4)
+                    s += indent_string(reg.name.upper() + "_" + field.name.upper() + "_RESET = ", 2)
                     s += field.reset + "\n"
                     s += "\n"
 
