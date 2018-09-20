e52d5e3f7be9e54247c79e392a5634d9993a16b1
ogre <olagrottvik@gmail.com>
2018-09-20 20:26:22 +0200

    Bump version: 0.6.2 → 0.6.3-dev

--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ except (IOError, ImportError):
 
 setup(
     name='uart',
-    version='0.6.2',
+    version='0.6.3-dev',
     packages=['uart'],
     license='MIT',
     install_requires=[
         'curses-menu==0.5.0',
-        'docopt==0.6.2',
+        'docopt==0.6.3-dev',
         'prettytable==0.7.2',
         'pylatexenc==1.2',
         'pypandoc==1.4',
