ee04f068d2fde56a378ed920c305eedab7ad91ae
Ola Groettvik <ola.groettvik@cern.ch>
2024-05-26 15:49:59 +0200

    Fixes

--- example/example_axi/header/example_axi.py
+++ example/example_axi/header/example_axi.py
@@ -1,37 +1,36 @@
 class EXAMPLE_AXI_H:
 
     """Register: reg0"""
-
     REG0_OFFSET = 0x0
     REG0_RESET = 0x0
 
-    """ Register: reg1 """
+    """Register: reg1"""
     REG1_OFFSET = 0x4
     REG1_RESET = 0x1
 
-    """ Register: reg2 """
+    """Register: reg2"""
     REG2_OFFSET = 0x8
     REG2_RESET = 0x0
 
-    """ Register: reg3 """
-    REG3_OFFSET = 0xC
+    """Register: reg3"""
+    REG3_OFFSET = 0xc
     REG3_RESET = 0x3
 
-    """ Register: reg4 """
+    """Register: reg4"""
     REG4_OFFSET = 0x10
     REG4_RESET = 0x0
 
-    """ Register: reg5 """
+    """Register: reg5"""
     REG5_OFFSET = 0x14
-    REG5_RESET = 0xFFFFFFFF
+    REG5_RESET = 0xffffffff
 
-    """ Register: reg6 """
+    """Register: reg6"""
     REG6_OFFSET = 0x18
     REG6_RESET = 0x0
 
-    """ Register: reg7 """
-    REG7_OFFSET = 0x1C
-    REG7_RESET = 0xAD7
+    """Register: reg7"""
+    REG7_OFFSET = 0x1c
+    REG7_RESET = 0xad7
 
     """ Field: field0 """
     REG7_FIELD0_OFFSET = 0
@@ -42,8 +41,8 @@ class EXAMPLE_AXI_H:
     """ Field: field1 """
     REG7_FIELD1_OFFSET = 1
     REG7_FIELD1_WIDTH = 4
-    REG7_FIELD1_RESET = 0xB
-    REG7_FIELD1_MASK = 0x1E
+    REG7_FIELD1_RESET = 0xb
+    REG7_FIELD1_MASK = 0x1e
 
     """ Field: field2 """
     REG7_FIELD2_OFFSET = 5
@@ -54,10 +53,10 @@ class EXAMPLE_AXI_H:
     """ Field: field3 """
     REG7_FIELD3_OFFSET = 6
     REG7_FIELD3_WIDTH = 15
-    REG7_FIELD3_RESET = 0x2B
-    REG7_FIELD3_MASK = 0x1FFFC0
+    REG7_FIELD3_RESET = 0x2b
+    REG7_FIELD3_MASK = 0x1fffc0
 
-    """ Register: reg8 """
+    """Register: reg8"""
     REG8_OFFSET = 0x20
     REG8_RESET = 0x0
 
@@ -71,7 +70,7 @@ class EXAMPLE_AXI_H:
     REG8_FIELD1_OFFSET = 1
     REG8_FIELD1_WIDTH = 19
     REG8_FIELD1_RESET = 0x0
-    REG8_FIELD1_MASK = 0xFFFFE
+    REG8_FIELD1_MASK = 0xffffe
 
     """ Field: field2 """
     REG8_FIELD2_OFFSET = 20
@@ -83,25 +82,25 @@ class EXAMPLE_AXI_H:
     REG8_FIELD3_OFFSET = 21
     REG8_FIELD3_WIDTH = 3
     REG8_FIELD3_RESET = 0x0
-    REG8_FIELD3_MASK = 0xE00000
+    REG8_FIELD3_MASK = 0xe00000
 
-    """ Register: reg9 """
+    """Register: reg9"""
     REG9_OFFSET = 0x24
     REG9_RESET = 0x1
 
-    """ Register: reg10 """
+    """Register: reg10"""
     REG10_OFFSET = 0x28
-    REG10_RESET = 0xA
+    REG10_RESET = 0xa
 
-    """ Register: reg11 """
-    REG11_OFFSET = 0x2C
+    """Register: reg11"""
+    REG11_OFFSET = 0x2c
     REG11_RESET = 0x3
 
     """ Field: field0 """
     REG11_FIELD0_OFFSET = 0
     REG11_FIELD0_WIDTH = 15
     REG11_FIELD0_RESET = 0x3
-    REG11_FIELD0_MASK = 0x7FFF
+    REG11_FIELD0_MASK = 0x7fff
 
     """ Field: field1 """
     REG11_FIELD1_OFFSET = 15
