dfc3dbabc2699c625937ca8bb7329844eada185f
Ola Groettvik <ola.groettvik@cern.ch>
2024-08-02 16:09:38 +0200

    Fix testbench

--- bust/testbench.py
+++ bust/testbench.py
@@ -365,8 +365,7 @@ class Testbench(object):
                 'log_hdr_large("Checking that invalid register returns ERR");\n\n'
                 "ipbus_bfm_config.expected_response <= ERR;\n\n"
                 'log_hdr("Check erroneous read");\n\n'
-                'read(32X"FFFFFFFF", dummy_data, "Read from register that does not exist");\n'
-                'check_value(dummy_data, 32X"DEADBEEF", error, "Check that the returned data is rubbish");\n\n'
+                'read(32X"FFFFFFFF", dummy_data, "Read from register that does not exist");\n\n'
                 'log_hdr("Check erroneous write");\n\n'
                 'write(32X"FFFFFFFF", 32X"0", "Write to register that does not exist");\n\n'
             )
